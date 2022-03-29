6a8f5617ba1a1769e5e0c9a26a379a7ccf6880a4
TomRaaff <tom.raaff@divotion.com>
2022-03-29 22:03:21 +0200

    experimentation with HTML forms

---
+++ tryout/html-validation/html-validation.ts
@@ -0,0 +1,41 @@
+function handleData() {
+    const name = document.querySelector('[id="name"]');
+    const age = document.querySelector('[id="age"]');
+    const email = document.querySelector('[id="email"]');
+    const color = document.querySelector('[id="color"]');
+    const birthday = document.querySelector('[id="birthday"]');
+    console.log({name, age, email, color, birthday});
+}
+
+function formToObject(form?: FormData): unknown {
+    const dataObj: any = {};
+    if (form) {
+        // @ts-ignore
+        for (let [key, value] of form.entries()) {
+            dataObj[key] = value;
+        }
+    }
+    return dataObj;
+}
+
+function setVisible(selector: string, isVisible: boolean) {
+    (document.querySelector(selector) as HTMLElement).style.display = (isVisible) ? '' : 'none';
+}
+
+function addEvent(selector: string, event: string, handler: (event: Event) => void) {
+    document.querySelector(selector)?.addEventListener(event, handler);
+}
+
+const form = document.querySelector('form');
+const formData = (form) ? new FormData(form) : undefined;
+
+addEvent('form', 'submit', (event) => {
+    event.preventDefault();
+    handleData();
+    console.log(formToObject(formData));
+});
+
+addEvent('#extraQuestion', 'change', (event) => {
+    const addExtraQuestion = (event.target as HTMLInputElement).checked;
+    setVisible('.petName', addExtraQuestion);
+});
